e36955cdd7ee44301e35f8743172a12ec92b550e
yfan <351793051@qq.com>
2022-06-23 23:01:03 +0800

    some change

--- app.py
+++ app.py
@@ -8,18 +8,18 @@ import pandas as pd
 #标题
 st.title('kTrainer')
 
-#设置持有天数的session_state
-if 'count' not in st.session_state:
-    st.session_state.count = 0
+##设置持有天数的session_state
+#if 'count' not in st.session_state:
+#    st.session_state.count = 0
 #设置买入标志位，用于判断股票的模拟买入状态
 if 'isbought' not in st.session_state:
     st.session_state.isbought = False
 
 
-#TODO:增加可调参数
+#TODO:增加板块数据
 @st.cache
-def load_data():
-    df = ak.stock_zh_a_hist(symbol="000001",period="daily",adjust="qfq")
+def load_data(name):
+    df = ak.stock_zh_a_hist(symbol=name,period="daily",adjust="qfq")
     #数据处理
     df["日期"] = pd.to_datetime(df["日期"])#修改日期为坐标
     df.set_index("日期", inplace=True)
@@ -37,20 +37,30 @@ def load_data():
                                         
     return df
 
-df = load_data()
-if st.checkbox('show data'):
-    df
 
+#TODO:初始化持有天数为0 !
 with st.sidebar:
     time_interval = st.slider('显示K线区间(天)', 30,720)
     date_to = st.date_input('选择某一天', date(2021, 10, 1))
-    doi = st.number_input('持有天数',min_value=0,step=1, value=st.session_state.count)
-    
-#TODO:忽略假期,用好loc
-df1 = df.loc[:date_to+timedelta(days=st.session_state.count)]
-df2 = df1.tail(time_interval)
-df2
+    doi = st.number_input('持有天数',min_value=0,step=1)
+    name = st.text_input('股票代码')
+    df = load_data(name)
 
+#TODO:显示股票名称
+#stock_individual_info_em_df = ak.stock_individual_info_em(symbol="000001")
+#st.subheader(stock_individual_info_em_df.loc['股票简称'])
+
+if st.checkbox('show data'):
+    df
+    
+
+#忽略假期
+bd = pd.tseries.offsets.BusinessDay(doi)
+df1 = df.loc[:date_to+bd]
+df2 = df1.tail(time_interval)
+
+
+#TODO:增加legend
 # 绘图
 #fig,ax = mpf.plot(df2, type="candle", title="Candlestick for MSFT", ylabel="price($)",mav=(5,21,55),volume=True,figratio=(2, 1),returnfig=True)
 #
@@ -60,7 +70,7 @@ fig,ax = mpf.plot(df2, addplot=ap, type='candle', volume=True, figratio=(2,1), r
 
 st.pyplot(fig)
 
-#买入卖出逻辑
+#TODO:买入卖出逻辑
 if st.session_state.isbought:       #如果已经买入
     btn1='买入'                     #显示持有按钮
     btn2='观望'                     #显示卖出按钮
@@ -75,6 +85,7 @@ if st.button(btn2):
     st.session_state.isbought = False   #买入标记为假
     st.session_state.count = 0          #持有天数清零
 
+#TODO:显示最新的持有日期 !
 #TODO:计算收益率
 st.write('收益率计算')
 
